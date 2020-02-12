844f831ece8d205b6429957e6ec287319c5e62b7
TheFifthUnknown <60981724+TheFifthUnknown@users.noreply.github.com>
2020-02-13 00:36:29 +0700

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.file_location = new System.Windows.Forms.Button();
+            this.review = new System.Windows.Forms.Button();
+            this.done = new System.Windows.Forms.Button();
+            this.way_fileCopy = new System.Windows.Forms.TextBox();
+            this.way_filePaste = new System.Windows.Forms.TextBox();
+            this.agree = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(12, 70);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(247, 23);
+            this.progress.TabIndex = 0;
+            // 
+            // file_location
+            // 
+            this.file_location.Location = new System.Drawing.Point(265, 10);
+            this.file_location.Name = "file_location";
+            this.file_location.Size = new System.Drawing.Size(75, 23);
+            this.file_location.TabIndex = 1;
+            this.file_location.Text = "File_location";
+            this.file_location.UseVisualStyleBackColor = true;
+            this.file_location.Click += new System.EventHandler(this.file_location_Click);
+            // 
+            // review
+            // 
+            this.review.Location = new System.Drawing.Point(265, 42);
+            this.review.Name = "review";
+            this.review.Size = new System.Drawing.Size(75, 23);
+            this.review.TabIndex = 2;
+            this.review.Text = "review";
+            this.review.UseVisualStyleBackColor = true;
+            this.review.Click += new System.EventHandler(this.review_Click);
+            // 
+            // done
+            // 
+            this.done.Location = new System.Drawing.Point(265, 70);
+            this.done.Name = "done";
+            this.done.Size = new System.Drawing.Size(75, 23);
+            this.done.TabIndex = 3;
+            this.done.Text = "do";
+            this.done.UseVisualStyleBackColor = true;
+            this.done.Click += new System.EventHandler(this.done_Click);
+            // 
+            // way_fileCopy
+            // 
+            this.way_fileCopy.Location = new System.Drawing.Point(12, 12);
+            this.way_fileCopy.Name = "way_fileCopy";
+            this.way_fileCopy.Size = new System.Drawing.Size(247, 20);
+            this.way_fileCopy.TabIndex = 4;
+            // 
+            // way_filePaste
+            // 
+            this.way_filePaste.Location = new System.Drawing.Point(12, 44);
+            this.way_filePaste.Name = "way_filePaste";
+            this.way_filePaste.Size = new System.Drawing.Size(247, 20);
+            this.way_filePaste.TabIndex = 5;
+            // 
+            // agree
+            // 
+            this.agree.AutoSize = true;
+            this.agree.Location = new System.Drawing.Point(12, 99);
+            this.agree.Name = "agree";
+            this.agree.Size = new System.Drawing.Size(143, 17);
+            this.agree.TabIndex = 6;
+            this.agree.Text = "I\'am agree with condition";
+            this.agree.UseVisualStyleBackColor = true;
+            this.agree.CheckedChanged += new System.EventHandler(this.agree_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 134);
+            this.Controls.Add(this.agree);
+            this.Controls.Add(this.way_filePaste);
+            this.Controls.Add(this.way_fileCopy);
+            this.Controls.Add(this.done);
+            this.Controls.Add(this.review);
+            this.Controls.Add(this.file_location);
+            this.Controls.Add(this.progress);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.Button file_location;
+        private System.Windows.Forms.Button review;
+        private System.Windows.Forms.Button done;
+        private System.Windows.Forms.TextBox way_fileCopy;
+        private System.Windows.Forms.TextBox way_filePaste;
+        private System.Windows.Forms.CheckBox agree;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            done.Enabled = false;
+        }
+
+        private void review_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                way_filePaste.Text = folder.SelectedPath;
+            }
+        }
+
+        private void file_location_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog folder = new OpenFileDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                way_fileCopy.Text = folder.FileName;
+            }
+        }
+        private string fileName(){
+            string fileName="";
+            string start = way_fileCopy.Text;
+  
+            int i = start.Length;
+            while (start.Substring(i - 1, 1)!="\\")
+            {
+                i--;
+                fileName = start.Substring(i - 1, 1)+fileName;
+                
+            }
+            return fileName;
+        }
+        private void done_Click(object sender, EventArgs e)
+        {
+            Copy(way_fileCopy.Text, way_filePaste.Text+fileName());
+        }
+
+        private void agree_CheckedChanged(object sender, EventArgs e)
+        {
+            if (agree.Checked)
+            {
+                done.Enabled = true;
+            }
+            else
+            {
+                done.Enabled = false;
+            }
+        }
+        private void Copy(string SourceFilePath,string DestFilePath)
+        {
+            byte[] buffer = new byte[1024 * 1024]; // 1MB buffer
+            bool cancelFlag = false;
+
+            using (var sourceStream = new FileStream(SourceFilePath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.Asynchronous | FileOptions.SequentialScan))
+
+            {
+                long fileLength = sourceStream.Length;
+                using (var destinationStream = new FileStream(DestFilePath, FileMode.CreateNew, FileAccess.Write, FileShare.None, 4096, FileOptions.Asynchronous | FileOptions.SequentialScan))
+                {
+                    long totalBytes = 0;
+                    int currentBlockSize = 0;
+
+                    while ((currentBlockSize = sourceStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        totalBytes += currentBlockSize;
+                        int persentage = (int)((double)totalBytes * 100.0 / fileLength);
+
+                        destinationStream.Write(buffer, 0, currentBlockSize);
+
+                        cancelFlag = false;
+                        progress.Value = persentage;
+
+                        if (cancelFlag == true)
+                        {
+                            //еще не закончено
+                            //тут удаление файла
+                            //программа копирования
+                            break;
+                        }
+                    } 
+                }
+            }
+            MessageBox.Show("the file was copied " + DestFilePath, "Done", MessageBoxButtons.OK);
+        }
+    }
+}
